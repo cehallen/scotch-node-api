9d03b11267cb67ff407aec3c06c49c2f226f4eb1
Chris Allen <cehallen@gmail.com>
2015-01-12 21:33:48 -0500

    Add Bear model and mongoose setup

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/ca-test1');
+var Bear = require('./app/models/bear');
+
 var router = express.Router();
 
 // Test route tested with postman
